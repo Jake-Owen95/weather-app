2005c78ec8a63d850f12a7936cbff103496a417d
Jake <jakejohnowen@gmail.com>
2021-06-30 09:18:55 +0200

    creating app bones

---
+++ fetch.js
@@ -0,0 +1,17 @@
+class Fetch {
+    async getCurrent(input) {
+      const myKey = "94b12357324404181f801d79d1cf815b";
+  
+      //make request to url
+  
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${myKey}`
+      );
+  
+      const data = await response.json();
+  
+      console.log(data);
+  
+      return data;
+    }
+  }
